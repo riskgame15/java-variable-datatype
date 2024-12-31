a0ee89085769f96ace8cc68e3cb2d966768b496d
riskgame15 <tttungcr@gmail.com>
2024-12-31 11:11:54 +0700

    variable datatype

---
+++ src/VariableDataType.java
@@ -0,0 +1,18 @@
+public class VariableDataType {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 1.0f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'A';
+        String s = "Hello, World!";
+
+        System.out.println("Integer: " + i);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Boolean: " + b);
+        System.out.println("Character: " + c);
+        System.out.println("String: " + s);
+        }
+    }
+
